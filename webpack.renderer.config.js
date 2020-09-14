f4be87e09f37126c1630108964041f593741c4d1
Vu Duy Tuan Anh <vuduytuananh@gmail.com>
2020-09-14 22:02:45 +0200

    Add webpack react fast refresh

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,12 +1,33 @@
 const rules = require('./webpack.rules');
 const plugins = require('./webpack.plugins');
+const ReactRefreshPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const isDevelopment = process.env.NODE_ENV !== 'production';
 
-rules.push({
-  test: /\.css$/,
-  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
-});
+rules.push(
+  {
+    test: /\.css$/,
+    use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+  },
+  {
+    test: /\.tsx?$/,
+    exclude: /(node_modules|\.webpack)/,
+    use: [
+      isDevelopment && {
+        loader: 'babel-loader',
+        options: { plugins: ['react-refresh/babel'] },
+      },
+      'ts-loader',
+    ].filter(Boolean),
+  }
+);
+
+plugins.push(
+  isDevelopment && new ReactRefreshPlugin()
+)
+plugins.filter(Boolean);
 
 module.exports = {
+  target: 'web',
   module: {
     rules,
   },
